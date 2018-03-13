2936e418bef0841b2aa3cb0d1da5d6d76cc44113
Artur Fogiel <arturprojekty1988@gmail.com>
2018-03-13 20:21:26 +0100

    Refactor

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-#!/usr/bin/env python
-
-from modules import downloader
-from modules import hosts_sources
 from modules import hosts_manager
-
-
-def files(path):
-    import os
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
+from modules import downloader
 
 
 def main():
-    dl = downloader.Downloader()
-    i = 0
-    for link in hosts_sources.HostsSources.get_links():
-        dl.download_file(link, filename="hosts_" + str(i) + ".txt")
-        i += 1
-
-    #
     hm = hosts_manager.HostsManager()
-    for file in files("."):
-        hm.import_host_file(file)
+    dl = downloader.HTTPDownloader()
+
+    dl.download(url='http://localhost/hosts.txt', file_path='/tmp/hosts.txt')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- modules/downloader.py
+++ modules/downloader.py
@@ -1,42 +1,83 @@
 import requests
+from enum import Enum
 import os
-from urllib.parse import urlparse
+from threading import Thread
 
 
-class Downloader:
+class WorkMode(Enum):
+    NORMAL = 0
+    STOP = 1
+    # same as stop, but also removes file
+    CANCEL = 2
 
+
+class HTTPDownloader:
     def __init__(self):
-        self.req = None
-        self.download_dir = Downloader.get_current_dir() + "/dl/"
+        self.chunk_bytes = 4096
+        self.total_bytes = 0
+        self.downloaded_bytes = 0
+        self.work = True
+        self.url = ''
+        self.file_path = ''
+        self.threads = []
+        self.response = None
 
-    @staticmethod
-    def get_current_dir() -> str:
-        return os.getcwd()
+    def clean(self):
+        self.total_bytes = 0
+        self.downloaded_bytes = 0
+        self.work = True
 
-    def set_download_dir(self, path: str) -> None:
-        self.download_dir = path
+    def create_empty_file(self, file_path: str):
+        fp = open(file_path, "wb")
+        fp.write('\0' * self.total_bytes)
+        fp.close()
 
-    @staticmethod
-    def get_filename_from_url(url: str) -> str:
-        parsed = urlparse(url)
-        return os.path.basename(parsed.path)
+    def download(self, url: str, file_path: str) -> None:
+        r"""Downloads file from HTTP server.
+
+        :param url: URL of file.
+        :param file_path: path to file
+        :return: :bool object
+        :rtype: bool
+        """
+        self.clean()
+        self.url = url
+        self.file_path = file_path
+
+        response = requests.get(self.url, stream=True)
+        total_length = response.headers.get('content-length')
+
+        if total_length is not None:
+            self.total_bytes = int(total_length)
+
+            print("starting thread")
+            self.threads.append(Thread(target=self.dl()))
+            self.threads[0].start()
+            self.threads[0].join()
+
+    def dl(self):
+        with open(self.file_path, "wb") as file:
+            print("Downloading %s" % self.file_path)
+
+            self.downloaded_bytes = 0
+            for data in self.response.iter_content(chunk_size=self.chunk_bytes):
+                # continue ?
+                if self.work == WorkMode.STOP or self.work == WorkMode.CANCEL:
+                    self.clean()
+                    file.close()
+                    if self.work == WorkMode.CANCEL:
+                        os.unlink(self.file_path)
+                    break
+
+                self.downloaded_bytes += len(data)
+                file.write(data)
+
+    def stop(self):
+        self.work = WorkMode.STOP
+
+    def cancel(self):
+        self.work = WorkMode.CANCEL
 
     @property
-    def get_download_dir(self) -> str:
-        return self.download_dir
-
-    def send_request(self, url: str) -> None:
-        # print("Sending request: " + url)
-        self.req = requests.get(url)
-        # print(self.req.status_code)
-        # print(self.req.headers['content-type'])
-        # print(self.req.encoding)
-
-    def download_file(self, url, filename=None):
-        self.send_request(url)
-        if filename is None:
-            filename = self.get_filename_from_url(url)
-        output_path = self.get_download_dir + filename
-        print("Downloading to: " + output_path)
-        with open(output_path, 'wb') as f:
-            f.write(self.req.content)
+    def get_progress(self) -> []:
+        return [self.total_bytes, self.downloaded_bytes]

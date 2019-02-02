f4afd1c5a9410debe3f270a19e1afc534ee0fc93
Artur Fogiel <arturprojekty1988@gmail.com>
2019-02-02 16:30:29 +0100

    Changed libname to xenial compatible
    Crated iptables rule in install.sh that redirects traffic to pydnshelper

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 rm -rf venv
 mkdir venv
 sudo apt install -y python-pip 
-sudo apt install -y authbind # to allow non-root users listen on port 53
+# sudo apt install -y authbind # to allow non-root users listen on port 53
 # authbind configuration 
-sudo touch /etc/authbind/byport/53
-sudo chmod 500 /etc/authbind/byport/53
+#sudo touch /etc/authbind/byport/53
+#sudo chmod 500 /etc/authbind/byport/53
 #
+#sudo iptables -t nat -A OUTPUT -o lo -p udp --dport 53 -j REDIRECT --to-port 5053
 pip install virtualenv
 python3 -m virtualenv -p /usr/bin/python3 venv
